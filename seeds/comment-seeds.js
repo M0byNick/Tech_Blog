69f12dd11a3e1ec0938d41b23d79ed9ba4bfad1a
Nicholas Morihisa <nicholasmorihisa@nicholaacstudio.mynetworksettings.com>
2022-08-03 21:32:38 -0400

    comment seeds

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,40 @@
+const { Comment } = require('../models');
+
+//All comment text is randomly generated
+const commentData = [
+    {
+        comment_text: 'Now for manners use has company believe parlors.',
+        post_id: 3,
+        user_id: 3
+    },
+    {
+        comment_text: 'Excellent you did therefore perfectly supposing described.',
+        post_id: 2,
+        user_id: 5
+    },
+    {
+        comment_text: 'Admiration considered acceptance too led one melancholy expression.',
+        post_id: 4,
+        user_id: 1
+    },
+    {
+        comment_text: 'Chamber hearing inhabit joy highest private ask him our believe.',
+        post_id: 1,
+        user_id: 3
+    },
+    {
+        comment_text: 'But why smiling man her imagine married.',
+        post_id: 4,
+        user_id: 2
+    },
+    {
+        comment_text: 'Improved own provided blessing may peculiar domestic.',
+        post_id: 5,
+        user_id: 4
+    },
+    {
+        comment_text: 'Evil mrs true get post.',
+        post_id: 1,
+        user_id: 5
+    },
+]
